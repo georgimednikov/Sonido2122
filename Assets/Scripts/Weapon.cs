21850286d4a16e4b8c7d599ba28b89ed2841ca75
Daniel González Cerdeiras <daniel20@ucm.es>
2021-12-06 18:43:19 +0100

    Primeros sonidos tanque

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using FMODUnity;
 public class Weapon : MonoBehaviour
 {
     public KeyCode fireKey;
@@ -10,11 +10,14 @@ public class Weapon : MonoBehaviour
     public Terraformer terraformer;
     public bool partOfAnArray = false;
     public float strength, terraformStrength, terraformRadius, fireRate;
+
+    StudioEventEmitter soundEvent;
     float lastShoot;
     // Start is called before the first frame update
     void Start()
     {
         lastShoot = Time.time;
+        soundEvent = GetComponent<StudioEventEmitter>();
     }
 
     // Update is called once per frame
@@ -30,11 +33,11 @@ public class Weapon : MonoBehaviour
     public void Shoot()
     {
         GameObject go = Instantiate(bullet, firePoint.transform.position + firePoint.transform.forward * transform.localScale.z, firePoint.transform.rotation);
-        //firePoint.GetComponent<AudioSource>()?.Play();
         go.GetComponent<Rigidbody>().AddForce(firePoint.transform.forward * strength, ForceMode.Impulse);
         DestroyOnHit hit = go.GetComponent<DestroyOnHit>();
         hit.terraformer = terraformer;
         hit.strength = terraformStrength;
         hit.radius = terraformRadius;
+        soundEvent.Play();
     }
 }
